8ee96ecaae9be873136023bf4c515ae77a3ee2fc
tarberd <bernardo.mferrari@gmail.com>
2020-10-13 16:19:13 -0300

    fix module assignment

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,36 @@
 #![no_std]
-pub mod uart;
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test_runner)]
+
+#[cfg(test)]
+fn test_runner(tests: &[&dyn Fn()]) {
+    println!("Running {} tests", tests.len());
+    for test in tests {
+        test();
+    }
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($args:tt)+) => {{
+        let mut uart = $crate::uart::Uart::new(0x1000_0000);
+        use core::fmt::Write;
+        let _ = write!(uart, $($args)+);
+    }};
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {{
+        print!("\r\n")
+    }};
+    ($fmt:expr) => {{
+        print!(concat!($fmt, "\r\n"))
+    }};
+    ($fmt:expr, $($args:tt)+) => {{
+        print!(concat!($fmt, "\r\n"), $($args)+)
+    }};
+}
+
 pub mod assignment;
+pub mod uart;
