9a85cf0d9a520ff77081e9b780a4fd357b3d72b3
tarberd <bernardo.mferrari@gmail.com>
2020-10-14 21:02:04 -0300

    Add page descriptor, page system init

--- src/assembly/mod.rs
+++ src/assembly/mod.rs
@@ -1,2 +1,18 @@
 global_asm!(include_str!("entry.S"));
 global_asm!(include_str!("memory.S"));
+
+extern "C" {
+    pub static BSS_START: usize;
+    pub static BSS_END: usize;
+    pub static HEAP_START: usize;
+    pub static HEAP_SIZE: usize;
+    pub static TEXT_START: usize;
+    pub static TEXT_END: usize;
+    pub static DATA_START: usize;
+    pub static DATA_END: usize;
+    pub static RODATA_START: usize;
+    pub static RODATA_END: usize;
+    pub static KERNEL_STACK_START: usize;
+    pub static KERNEL_STACK_END: usize;
+}
+
