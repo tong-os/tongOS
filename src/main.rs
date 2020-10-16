d64924c3589a4aba7bfb2f4abbf1c1d495aebffc
tarberd <bernardo.mferrari@gmail.com>
2020-10-16 17:18:02 -0300

    add global allocator

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,20 @@
     asm,
     global_asm,
     allocator_api,
+    alloc_prelude,
     alloc_error_handler,
-    const_raw_ptr_to_usize_cast,
     lang_items,
     custom_test_frameworks
 )]
 #![test_runner(crate::test_runner)]
 
 use tong_os::{print, println};
+
+
+#[macro_use]
+extern crate alloc;
+
+use alloc::prelude::v1::*;
 
 #[cfg(test)]
 fn test_runner(tests: &[&dyn Fn()]) {
@@ -50,4 +56,45 @@ extern "C" fn abort() -> ! {
     }
 }
 
-mod kinit;
+use tong_os::assembly::*;
+use tong_os::assignment;
+
+#[no_mangle]
+extern "C" fn kinit(_hartid: usize) -> ! {
+    tong_os::uart::Uart::new(0x1000_0000).init();
+    print!("Set all bytes in BSS to zero ...");
+    for address in unsafe { BSS_START..BSS_END } {
+        unsafe {
+            (address as *mut usize).write_volatile(0);
+        }
+    }
+    println!("Finished!");
+    println!("Init tests!");
+    assignment::print_sections();
+    assignment::test_bss();
+
+    println!("Init pages");
+    tong_os::page::init();
+    tong_os::page::print_page_allocations();
+    tong_os::kmem::init();
+    tong_os::kmem::print_table();
+    let x = vec![0, 1, 2, 3];
+    tong_os::kmem::print_table();
+
+    println!("Finished!");
+
+    println!("You are now in ...");
+    println!(concat!(
+        " _                    _____ _____ \n",
+        "| |                  |  _  /  ___|\n",
+        "| |_ ___  _ __   __ _| | | \\ `--. \n",
+        "| __/ _ \\| '_ \\ / _` | | | |`--. \\\n",
+        "| || (_) | | | | (_| \\ \\_/ /\\__/ /\n",
+        " \\__\\___/|_| |_|\\__, |\\___/\\____/ \n",
+        "                 __/ | \n",
+        "                |___/ ",
+    ));
+
+
+    loop {}
+}
