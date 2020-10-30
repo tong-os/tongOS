70f4ede0ebf333006d3e7dd71a3fdaa9aacf0564
tarberd <bernardo.mferrari@gmail.com>
2020-10-30 19:11:08 -0300

    Add timer interrupt

--- src/trap.rs
+++ src/trap.rs
@@ -1,11 +1,46 @@
+// trap.rs
+// Trap routines
+// Stephen Marz
+// tongOS team
+
+use crate::cpu::CONTEXT_SWITCH_TIME;
 use crate::process::{self, Process};
 use crate::scheduler::schedule;
-use alloc::boxed::Box;
 
 pub fn init() {
     use crate::assembly::__tong_os_trap;
+
+    let mstatus: usize;
+    unsafe { asm!("csrr {}, mstatus", out(reg) mstatus) }
+
+    // [3] = MIE (Machine Interrupt Enable)
+    let flags = 1 << 3;
+    let mstatus = mstatus | flags;
+    unsafe { asm!("csrw mstatus, {}", in(reg) mstatus) }
+
+    let mie: usize;
+    unsafe { asm!("csrr {}, mie", out(reg) mie) }
+
+    // [7] = MTIE (Machine Time Interrupt Enable)
+    let flags = 1 << 7;
+    let mie = mie | flags;
+    unsafe { asm!("csrw mie, {}", in(reg) mie) }
+
+    unsafe { asm!("csrw mtvec, {}", in(reg) (__tong_os_trap as usize)) }
+}
+
+// CLINT Memory Map
+// https://sifive.cdn.prismic.io/sifive/b5e7a29c-d3c2-44ea-85fb-acc1df282e21_FU540-C000-v1p3.pdf
+pub const MMIO_MTIMECMP: *mut u64 = 0x0200_4000usize as *mut u64;
+pub const MMIO_MTIME: *const u64 = 0x0200_BFF8 as *const u64;
+
+pub fn schedule_machine_timer_interrupt(quantum: usize) {
     unsafe {
-        asm!("csrw mtvec, {}", in(reg) (__tong_os_trap as usize));
+        MMIO_MTIMECMP.write_volatile(
+            MMIO_MTIME
+                .read_volatile()
+                .wrapping_add(CONTEXT_SWITCH_TIME * quantum as u64),
+        );
     }
 }
 
@@ -24,6 +59,9 @@ pub fn tong_os_trap(process: &mut Process) {
 
     if is_async {
         match cause {
+            7 => {
+                panic!("Unhandled machine timer interrupt!!");
+            }
             _ => {
                 panic!(
                     "Unhandled async trap CPU#{} -> {}\n",
